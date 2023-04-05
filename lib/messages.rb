be08f7e78e5944a32bf498a60c34ac7fadc3e51d
Kelsey Roy <kroy@8thlight.com>
2023-04-05 12:28:03 -0700

    Main program uses Message

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,8 +1,8 @@
 class Message
 
     TTT_MESSAGES = {
-      welcome: "Welcome to Tic Tac Toe!",
-      instructions: "Tic Tac Toe is a two player game played on a 3 x 3 grid.  When prompted, each player will place their mark by entering a number between 1-9 that corresponds with an available square on the grid."
+      welcome: "Welcome to Tic Tac Toe!\n\n",
+      instructions: "Tic Tac Toe is a two player game played\non a 3 x 3 grid. When prompted, each\nplayer will place their mark by entering\na number between 1-9 that corresponds with\nan available square on the grid."
     }.freeze
 
   def initialize(messages=nil)

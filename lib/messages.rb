848ca6030e3dd326d42a563ee08b3a902bd3bb2a
Kelsey Roy <kroy@8thlight.com>
2023-04-05 12:19:38 -0700

    Refactored Message to take a hash of messages

--- lib/messages.rb
+++ lib/messages.rb
@@ -1,10 +1,13 @@
 class Message
 
-  def initialize
-    @messages = {
+    TTT_MESSAGES = {
       welcome: "Welcome to Tic Tac Toe!",
       instructions: "Tic Tac Toe is a two player game played on a 3 x 3 grid.  When prompted, each player will place their mark by entering a number between 1-9 that corresponds with an available square on the grid."
-    }
+    }.freeze
+
+  def initialize(messages=nil)
+    messages ||= TTT_MESSAGES
+    @messages = messages
   end
 
   def lookup(message)
--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -3,21 +3,22 @@ require './lib/messages'
 
 describe Message do
   
+  before(:all) do
+    @mock_messages = {test_message: "this is a test message"}
+    @message = Message.new(@mock_messages)
+  end
+
   describe "lookup" do
     it 'throws an error when a message is not defined' do
-      test_message = Message.new
-
       expect {
-        result = test_message.lookup(:does_not_exist)
+        result = @message.lookup(:does_not_exist)
       }.to raise_error(ArgumentError, "Message does not exist")     
     end
 
     it 'displays a message when a message is defined' do
-      test_message = Message.new
+      result = @message.lookup(:test_message)
 
-      result = test_message.lookup(:welcome)
-
-      expect(result).to eq "Welcome to Tic Tac Toe!" 
+      expect(result).to eq @mock_messages[:test_message] 
     end
   end
 end

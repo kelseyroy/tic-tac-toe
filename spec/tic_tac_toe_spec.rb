de4216af375bdc3061fece78699a97dc4ce2e265
Kelsey Roy <kroy@8thlight.com>
2023-04-07 15:31:19 -0700

    Changes language in display_board test

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -7,7 +7,7 @@ describe TicTacToe do
     @test_game = TicTacToe.new
   end
 
-  it 'can access draw_board from Board class' do
+  it 'can display the current board from Board class' do
     board = Board.new
     expect(@test_game.display_board).to eq "\n 1 | 2 | 3 \n===+===+===\n 4 | 5 | 6 \n===+===+===\n 7 | 8 | 9 \n\n"
   end

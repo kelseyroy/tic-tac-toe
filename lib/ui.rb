778e144389b7e806ee7a6b1ddf893ba669648d01
Kelsey Roy <kroy@8thlight.com>
2023-04-11 10:45:42 -0700

    Removes unused tests and moves format_board from ui.rb to board.rb

--- lib/ui.rb
+++ lib/ui.rb
@@ -2,20 +2,6 @@ require_relative 'messages'
 
 class UserInterface
 
-    def display_board(board)
-        line_break = "\n"
-        grid_lines = "===+===+==="
-        board_grid = line_break +
-        " #{board[0]} | #{board[1]} | #{board[2]} " + line_break +
-        grid_lines + line_break +
-        " #{board[3]} | #{board[4]} | #{board[5]} " + line_break +
-        grid_lines + line_break +
-        " #{board[6]} | #{board[7]} | #{board[8]} " + line_break +
-        line_break
-        
-        return board_grid
-    end
-
     def display(message)
         print message
     end
